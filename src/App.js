4227add7fbc7ad58df382dc6ce7edffccbd045b9
LarissaSimoes <larissacarneiro2@hotmail.com>
2023-02-09 20:51:28 -0300

    Requisito 5

--- src/App.js
+++ src/App.js
@@ -1,25 +1,46 @@
 import React, { Component } from 'react';
 import './App.css';
+import { getProductsFromCategoryAndQuery } from './services/api';
+import ProductCard from './components/ProductCard';
 
 export default class App extends Component {
-  state = { inputValue: '' };
+  state = {
+    inputValue: '',
+    productsList: [],
+  };
 
   onInputChange = ({ target }) => {
     this.setState({ inputValue: target.value });
   };
 
-  render() {
+  handleButton = async () => {
     const { inputValue } = this.state;
+    const products = await getProductsFromCategoryAndQuery('', inputValue);
+    this.setState({
+      productsList: products.results,
+    });
+  };
+
+  render() {
+    const { inputValue, productsList } = this.state;
     return (
       <div className="App">
         <label htmlFor="input-products">
           <input
+            data-testid="query-input"
             id="input-products"
             type="text"
             value={ inputValue }
             onChange={ this.onInputChange }
           />
         </label>
+        <button
+          data-testid="query-button"
+          type="button"
+          onClick={ this.handleButton }
+        >
+          Pesquisar
+        </button>
         {
           !inputValue && (
             <p data-testid="home-initial-message">
@@ -27,6 +48,15 @@ export default class App extends Component {
             </p>
           )
         }
+        <div>
+          { productsList.length > 0
+            ? productsList.map((product) => (
+              <ProductCard
+                key={ product.id }
+                product={ product }
+              />
+            )) : <h3>Nenhum produto foi encontrado</h3>}
+        </div>
       </div>
     );
   }
