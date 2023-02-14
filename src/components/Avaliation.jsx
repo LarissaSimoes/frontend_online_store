33c8ecdfe407c2c026ae3ad7999325356aa7370f
Arthur Garcia <arthur_hg@hotmail.com>
2023-02-13 23:10:07 -0300

    Início do Requisito 11

---
+++ src/components/Avaliation.jsx
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+class Avaliation extends Component {
+  const
+  render() {
+    return(
+      <form></form>
+    );
+  }
+}
+
+Avaliation.propTypes = {
+  xxxxx: PropTypes.string.isRequired,
+};
+
+export default Avaliation
