a552fceb715b8914bfa2abae7d22276ef6aedac3
Bruna Loyola <brunloyola02@gmail.com>
2023-02-15 19:39:04 -0300

    Tentativa req 08

--- src/pages/ShoppingCart.jsx
+++ src/pages/ShoppingCart.jsx
@@ -13,47 +13,28 @@ class ShoppingCart extends Component {
   };
 
   render() {
-    const products = JSON.parse(localStorage.getItem('products')) || [];
     const { cartProducts } = this.state;
-
-    // const emptyCartElement = cartProducts.length === 0 && (
-    //   <h3 data-testid="shopping-cart-empty-message">
-    //     Seu carrinho está vazio
-    //   </h3>
-    // );
+    const emptyCartElement = cartProducts.length === 0 && (
+      <h3 data-testid="shopping-cart-empty-message">
+        Seu carrinho está vazio
+      </h3>
+    );
 
     const cartProductsElement = cartProducts.map((product, index) => (
       <li key={ index }>
         <h3 data-testid="shopping-cart-product-name">{ product.name }</h3>
+        <img src={ product.image } alt={ product.name } />
+        <h3>
+          R$
+          {product.value}
+        </h3>
         <h3 data-testid="shopping-cart-product-quantity">{ product.qt }</h3>
-        <img src={ product.image } alt={ product.name } />
-        <h3>
-          R$
-          {product.value}
-        </h3>
       </li>
     ));
-    const productElement = products.map((product, index) => (
-      <li key={ index }>
-        <h2 data-testid="shopping-cart-product-name">{product.name}</h2>
-        <img src={ product.image } alt={ product.name } />
-        <h3>
-          R$
-          {product.value}
-        </h3>
-      </li>
-    ));
-    if (products.length === 0 || cartProducts.length === 0) {
-      return (
-        <p data-testid="shopping-cart-empty-message">Seu carrinho está vazio</p>
-      );
-    }
     return (
       <div>
+        {emptyCartElement}
         <ul>
-          <div>
-            {productElement}
-          </div>
           {cartProductsElement}
         </ul>
       </div>
